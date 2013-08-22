38924008067871905ed6735537789c1bcf127e0d
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2013-08-23 02:39:48 +0400

    ticket:1 created branch t1_contact test_home modified to fail (searching 'Name' in response); created makefile to running tests; added south to settings.py and requirements; removed fixture initial_data.json.

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -10,18 +10,9 @@ from django.test import TestCase
 from django.test.client import Client
 
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-
 class HttpTest(TestCase):
     def test_home(self):
         c = Client()
         response = c.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello!')
+        self.assertContains(response, 'Name')
