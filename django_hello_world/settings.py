a7ccf7cc475c8a4720a1d34a440b170d5a007d02
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2013-08-29 02:33:48 +0400

    ticket:5 improved makefile; migrated to django 5

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -50,7 +50,7 @@ MEDIA_ROOT = '/home/nedr/progs/django-hello-world/media'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:8000/media'
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -132,9 +132,15 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
