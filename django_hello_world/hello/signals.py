ffd091c99c67db5c0f67151356344d92abb90c21
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2013-09-09 22:47:03 +0400

    ticket:10 new behaviour on creating db: when doing loaddata or migrate - just use setting that turn off signal processors.

--- django_hello_world/hello/signals.py
+++ django_hello_world/hello/signals.py
@@ -1,27 +1,26 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django_hello_world.hello.models import Requests, State
-from django.db import DatabaseError
+from django.conf import settings
 
+if settings.ENABLE_DB_SIGNALS:
 
-@receiver(post_save)
-def limit_model(sender, **kwargs):
-    if sender == Requests:
-        count = Requests.objects.count()
-        if count > 15:
-            last_id = Requests.objects.values_list('id', flat=True)[count-1]
-            Requests.objects.filter(id__lte=last_id-15).delete()
-            if Requests.objects.count() > 15 and __debug__:
-                print 'count       ', count
-                print 'list id     ', Requests.objects.values_list('id', flat=True)
-                print 'list filtred', Requests.objects.filter(id__lt=count-14).values_list('id', flat=True)
-                print 'count_after ', Requests.objects.count()
+    @receiver(post_save)
+    def limit_model(sender, **kwargs):
+        if sender == Requests:
+            count = Requests.objects.count()
+            if count > 15:
+                last_id = Requests.objects.values_list('id', flat=True)[count-1]
+                Requests.objects.filter(id__lte=last_id-15).delete()
+                if Requests.objects.count() > 15 and __debug__:
+                    print 'count       ', count
+                    print 'list id     ', Requests.objects.values_list('id', flat=True)
+                    print 'list filtred', Requests.objects.filter(id__lt=count-14).values_list('id', flat=True)
+                    print 'count_after ', Requests.objects.count()
 
-
-@receiver([post_save, post_delete])
-def save_change_of_state(sender, **kwargs):
-    if sender != State:
-        try:
+    @receiver([post_save, post_delete])
+    def save_change_of_state(sender, **kwargs):
+        if sender != State:
             rec_id = kwargs['instance'].id
             if not 'created' in kwargs:  # this was deletion
                 State(record_id=rec_id, model=sender, state='deleted').save()
@@ -32,10 +31,3 @@ def save_change_of_state(sender, **kwargs):
             else:  # changing of existing record
                 State(record_id=rec_id, model=sender, state='changed').save()
                 print 'changed'
-        except TypeError:
-            pass
-        except AttributeError:
-            pass
-        except DatabaseError:
-            pass
-
