6294280093541df7d2d3a2177aeca32eb0e63fa3
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2013-09-09 21:26:44 +0400

    ticket:10 prettyprint of dump; exceptions

--- django_hello_world/hello/signals.py
+++ django_hello_world/hello/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django_hello_world.hello.models import Requests, State
+from django.db import DatabaseError
 
 
 @receiver(post_save)
@@ -20,15 +21,21 @@ def limit_model(sender, **kwargs):
 @receiver([post_save, post_delete])
 def save_change_of_state(sender, **kwargs):
     if sender != State:
-        rec_id = kwargs['instance'].id
-        if not 'created' in kwargs:  # this was deletion
-            State(record_id=rec_id, model=sender, state='deleted').save()
-            print 'deleted'
-        elif kwargs['created']:  # creation of new record
-            State(record_id=rec_id, model=sender, state='created').save()
-            print 'created'
-        else:  # changing of existing record
-            State(record_id=rec_id, model=sender, state='changed').save()
-            print 'changed'
-
+        try:
+            rec_id = kwargs['instance'].id
+            if not 'created' in kwargs:  # this was deletion
+                State(record_id=rec_id, model=sender, state='deleted').save()
+                print 'deleted'
+            elif kwargs['created']:  # creation of new record
+                State(record_id=rec_id, model=sender, state='created').save()
+                print 'created'
+            else:  # changing of existing record
+                State(record_id=rec_id, model=sender, state='changed').save()
+                print 'changed'
+        except TypeError:
+            pass
+        except AttributeError:
+            pass
+        except DatabaseError:
+            pass
 
