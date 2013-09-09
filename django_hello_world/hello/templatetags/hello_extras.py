1b7725a097990189e6dcd85ed0aea4baf594fde5
shadow_identitiy <pavel.nedr@gmail.com>
2013-09-09 08:48:49 +0400

    bugfixes: tag fixed, working well

--- django_hello_world/hello/templatetags/hello_extras.py
+++ django_hello_world/hello/templatetags/hello_extras.py
@@ -7,10 +7,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True, name='edit_link')
 def do_get_admin_link(context, record):
     """ create hyperlink to admin edit of record by uid """
-    #TODO: REMOVE THIS MOCK!!!
-    #rel_url = reverse('admin:hello_contact_change', args=(record.id,))
-    #abs_url = context['request'].build_absolute_uri(rel_url)
-    rel_url = reverse('admin:hello_contact_change', args=(1,))
+    rel_url = reverse('admin:hello_contact_change', args=(record,))
     abs_url = context['request'].build_absolute_uri(rel_url)
     return abs_url
 
