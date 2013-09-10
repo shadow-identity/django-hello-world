4fcd9d893abd489472ffb14b773574773aaaa73c
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2013-09-11 00:30:35 +0400

    ticket:3 methods and attributes is understanded

--- django_hello_world/hello/middleware.py
+++ django_hello_world/hello/middleware.py
@@ -1,8 +1,16 @@
 from django_hello_world.hello.models import Requests
-
+from datetime import datetime
 
 class HelloMiddlewares(object):
 
     def process_request(self, request):
         Requests(req=request).save()
+        print 'url      ', request.build_absolute_uri()
+        print 'datetime ', datetime.utcnow()
+        print 'method   ', request.method
+        if request.user.is_authenticated():
+            print 'user     ', request.user.username
+        else:
+            print 'user     ', 'anonymous'
+
         return
