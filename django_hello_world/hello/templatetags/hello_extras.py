579c1468c0ddfc536913e9049d37bd32f54f0826
shadow_identitiy <pavel.nedr@gmail.com>
2013-09-10 11:28:48 +0400

    ticket:8 fixed to give admin link to any object

--- django_hello_world/hello/templatetags/hello_extras.py
+++ django_hello_world/hello/templatetags/hello_extras.py
@@ -3,11 +3,9 @@ from django.core.urlresolvers import reverse
 
 register = template.Library()
 
-
 @register.simple_tag(takes_context=True, name='edit_link')
-def do_get_admin_link(context, record):
+def get_admin_link(context, record):
     """ create hyperlink to admin edit of record by uid """
-    rel_url = reverse('admin:hello_contact_change', args=(record,))
+    rel_url = reverse('admin:%s_%s_change' % (record._meta.app_label,  record._meta.module_name),  args=(record.id,))
     abs_url = context['request'].build_absolute_uri(rel_url)
     return abs_url
-
