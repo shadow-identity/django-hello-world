f972c8e2d12c923c43ff73d603d61d2a6b855e82
shadow_identitiy <pavel.nedr@gmail.com>
2013-10-04 14:28:29 +0400

    ticket:3 test_save_request_to_db: working with requests directly (instead of Client objects)

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -93,7 +93,7 @@ class HelloDBManipulationsTest(TestCase):
         """ Test that we really save requests to db
         """
         class MockUser():
-            """ mock class to imitate .user attribute in requests """
+            """ mock class to imitate .user.username attribute in requests """
             def __init__(self):
                 self.username = 'MockUser'
 
